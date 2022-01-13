f98ebf71c97397461d29ebf223ec8669e231d5da
Mathias <madr1402@hotmail.com>
2022-01-13 09:45:09 +0100

    first commit

---
+++ src/main/java/facades/FacadeExample.java
@@ -0,0 +1,49 @@
+package facades;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+//import errorhandling.RenameMeNotFoundException;
+import utils.EMF_Creator;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class FacadeExample {
+
+    private static FacadeExample instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private FacadeExample() {}
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static FacadeExample getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new FacadeExample();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        emf = EMF_Creator.createEntityManagerFactory();
+        FacadeExample fe = getFacadeExample(emf);
+       
+    }
+
+}
